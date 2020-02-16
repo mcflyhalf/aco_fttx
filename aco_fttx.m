52677de18198ea90e214e3fde9d5e1602c697ca6
Josiah <mcflyhalf@live.com>
2020-02-16 18:07:37 +0400

    Initial commit. Skeleton of classes

---
+++ aco_fttx.m
@@ -0,0 +1,41 @@
+% Procedure for ACO
+
+
+%Relevant Variables 
+% num_ants, input_csv_file, top_n (elitism) others
+% num_ants, input_csv_file, top_n (elitism) others
+
+
+
+% Create World
+
+
+%for max_iterations
+    %Create ants
+
+
+
+    %Let each ant find a solution
+
+
+
+    %Compare all ants' solutions and rank
+    %Also pick top n ants
+
+
+
+    %Update pheromones
+
+
+
+    %Save the top n ants
+
+
+
+    %kill all ants
+    
+    
+    
+%endfor
+
+%print top n solutions
