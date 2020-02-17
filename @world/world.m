125b281c04aabaf93c9d37c3ab68cc8b56693bb4
Khalai <rachedda@gmail.com>
2020-02-17 12:46:46 +0400

    variable creation
    I'm still struggling to understand how all those classes and functions work. So I just created the matrices directly. I can't get enough material to understand how it all works except for implementations that use sligthly different ideas than what you described... But here is my first attempt

--- @world/world.m
+++ @world/world.m
@@ -1,5 +1,26 @@
-function w = world(csv_filename, num_ants, elitism_num)
+function w = world(csv_filename, csv_filename2, num_ants, elitism_num)
+
+  num_ants = 10;
+  elitism_num = 5;
+  csv_filename = "HC_DistMat.csv";
+  csv_filename2 = "CS_DistMat.csv";
+
+  %Read Distance values from the csv files
+  HC_vector_Array= csvread('HC_DistMat.csv',1,5);  
+  CS_vector_Array= csvread('CS_DistMat.csv',1,5);  
+
+  % Find a way of determining size of homes, closures and splitters
+  % Homes=7 Closures=3 Splitters=3
+
+  DistHC_matrix = (reshape(HC_vector_Array,7,3))';
+  DistCS_matrix = (reshape(CS_vector_Array,3,3))';
+  
+  Pher_HC = ones(7,3);
+  Pher_CS = ones(3,3);
+
+
   w.file = csv_filename;
   w.x = num_ants;
   w.y = elitism_num;
+  w=class(w,"world");
 endfunction
